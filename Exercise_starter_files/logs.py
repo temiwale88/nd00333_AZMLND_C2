868710ece144fc31142ff17ecb3652f3d3111ae2
Elijah Adeoye <Temiwale88@gmail.com>
2020-12-27 01:17:44 -0800

    added swagger file

--- Exercise_starter_files/logs.py
+++ Exercise_starter_files/logs.py
@@ -5,11 +5,15 @@ from azureml.core.webservice import Webservice
 ws = Workspace.from_config()
 
 # Set with the deployment name
-name = ""
+name = "bikesharing-2-stackensemble"
 
 # load existing web service
 service = Webservice(name=name, workspace=ws)
+
+service.update(enable_app_insights=True)
+
 logs = service.get_logs()
 
 for line in logs.split('\n'):
     print(line)
+
